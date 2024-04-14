a29ee70413af97148eb69c1d465a7778cb3bc18c
speki <zspekt@tutanota.com>
2024-04-14 15:11:17 -0300

    feature: example migrations & queries and dir structure for goose & sqlc
    Signed-off-by: speki <zspekt@tutanota.com>

---
+++ sql/queries/users.sql
@@ -0,0 +1,14 @@
+-- example query (sqlc)
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, name, apikey)
+    VALUES ($1, $2, $3, $4, encode(sha256 (random()::text::bytea), 'hex'))
+RETURNING
+    *;
+
+-- name: GetAllByApiKey :one
+SELECT
+    *
+FROM
+    users
+WHERE
+    apikey = $1;
---
+++ sql/schema/001_users.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE users (
+    id uuid PRIMARY KEY,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    name text NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
+
+-- example migration
