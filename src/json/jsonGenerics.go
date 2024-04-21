2259ed4639793fe733d7392f4938caf129a45774
speki <zspekt@tutanota.com>
2024-04-21 13:22:06 -0300

    feat: replaced log with slog
    Signed-off-by: speki <zspekt@tutanota.com>

--- src/json/jsonGenerics.go
+++ src/json/jsonGenerics.go
@@ -3,31 +3,32 @@ package json
 import (
 	"encoding/json"
 	"io"
-	"log"
+	"log/slog"
 	"net/http"
 )
 
 // writes to $w a response with http status code $code and the json $payload
 func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	log.Printf("Responding with code %v and provided payload...\n", code)
+	slog.Info("responding with json", "code", code)
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
 
 	jsonPayload, err := json.Marshal(payload)
 	if err != nil {
-		log.Println(err)
+		slog.Error("error marshalling json payload", "error", err)
 		return
 	}
-
+	// should return status code after checking for an error
+	w.WriteHeader(code)
 	w.Write(jsonPayload)
 }
 
 // decodes reader with JSON-formatted content $r into the STRUCT pointed at by $st
 func DecodeJson[T any](r io.Reader, st *T) error {
 	decoder := json.NewDecoder(r)
+
 	err := decoder.Decode(st)
 	if err != nil {
-		log.Printf("\nError decoding json %v\n", err)
+		slog.Error("error decoding json", "error", err)
 		return err
 	}
 	return nil
